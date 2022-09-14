18a4c770feaab57c88e00321ded1bfa3d116f21a
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-09-14 17:23:33 +0200

    create structure

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="icon">
+    <img src="../assets/images/shared/logo.svg" alt="Logo de Space Tourism" />
+  </div>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/destination">Destination</RouterLink>
+    <RouterLink to="/crew">Crew</RouterLink>
+    <RouterLink to="/technology">Technology</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  color: var(--color-white);
+}
+</style>
