b1f9de09bc5653611ccebb4f75136a5eebfb348c
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-09-14 16:08:46 +0200

    add paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import DestinationView from "@/views/DestinationView.vue";
+import CrewView from "@/views/CrewView.vue";
+import TechnologyView from "@/views/TechnologyView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/destination",
+      name: "destination",
+      component: DestinationView,
+    },
+    {
+      path: "/crew",
+      name: "crew",
+      component: CrewView,
+    },
+    {
+      path: "/technology",
+      name: "technology",
+      component: TechnologyView,
     },
     {
       path: "/about",
