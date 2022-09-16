5924bebbe24c3d6cb96ee22342ec8d15ebf4bc78
Thomas Auffroy <thomas.auffroy@gmail.com>
2022-09-16 14:00:45 +0200

    create file

---
+++ src/components/DisplayTechnology.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <div class="content">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+    <img src="" alt="" />
+  </main>
+</template>
