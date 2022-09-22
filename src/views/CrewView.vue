8bbdfafe1e95820a79aea91e318667a70772b566
Thomas <thomas.auffroy@gmail.com>
2022-09-22 18:57:41 +0200

    add logic to dynamically display bg image

--- src/views/CrewView.vue
+++ src/views/CrewView.vue
@@ -1,6 +1,41 @@
 <script setup>
 import HeaderComponent from "../components/HeaderComponent.vue";
 import DisplayCrew from "../components/DisplayCrew.vue";
+
+import bgDesktop from "@/assets/images/crew/background-crew-desktop.jpg";
+import bgTablet from "@/assets/images/crew/background-crew-tablet.jpg";
+import bgMobile from "@/assets/images/crew/background-crew-mobile.jpg";
+
+import { onMounted, onUnmounted, onUpdated } from "vue";
+
+const props = defineProps({
+  windowFormat: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+
+onMounted(() => {
+  onPropsUpdate();
+});
+
+onUpdated(() => {
+  onPropsUpdate();
+});
+
+onUnmounted(() => {
+  document.body.style.backgroundImage = "";
+});
+function onPropsUpdate() {
+  if (props.windowFormat === "desktop") {
+    document.body.style.backgroundImage = `url(${bgDesktop})`;
+  } else if (props.windowFormat === "tablet") {
+    document.body.style.backgroundImage = `url(${bgTablet})`;
+  } else if (props.windowFormat === "mobile") {
+    document.body.style.backgroundImage = `url(${bgMobile})`;
+  }
+}
 </script>
 
 <template>
--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -1,6 +1,41 @@
 <script setup>
 import HeaderComponent from "../components/HeaderComponent.vue";
 import DisplayDestination from "../components/DisplayDestination.vue";
+
+import bgDesktop from "@/assets/images/destination/background-destination-desktop.jpg";
+import bgTablet from "@/assets/images/destination/background-destination-tablet.jpg";
+import bgMobile from "@/assets/images/destination/background-destination-mobile.jpg";
+
+import { onMounted, onUnmounted, onUpdated } from "vue";
+
+const props = defineProps({
+  windowFormat: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+
+onMounted(() => {
+  onPropsUpdate();
+});
+
+onUpdated(() => {
+  onPropsUpdate();
+});
+
+onUnmounted(() => {
+  document.body.style.backgroundImage = "";
+});
+function onPropsUpdate() {
+  if (props.windowFormat === "desktop") {
+    document.body.style.backgroundImage = `url(${bgDesktop})`;
+  } else if (props.windowFormat === "tablet") {
+    document.body.style.backgroundImage = `url(${bgTablet})`;
+  } else if (props.windowFormat === "mobile") {
+    document.body.style.backgroundImage = `url(${bgMobile})`;
+  }
+}
 </script>
 
 <template>
