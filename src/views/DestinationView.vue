c894b8d3170e978e6341eca4489c1314f5be8d61
Thomas <thomas.auffroy@gmail.com>
2022-09-22 18:48:11 +0200

    remove .background

--- src/views/DestinationView.vue
+++ src/views/DestinationView.vue
@@ -4,22 +4,13 @@ import DisplayDestination from "../components/DisplayDestination.vue";
 </script>
 
 <template>
-  <div class="background">
-    <HeaderComponent />
-    <h5><span>01</span> pick your destination</h5>
-    <DisplayDestination />
-  </div>
+  <HeaderComponent />
+  <h5><span>01</span> pick your destination</h5>
+  <DisplayDestination />
 </template>
 
 <style scoped>
-.background {
-  background-image: url("../assets/images/destination/background-destination-desktop.jpg");
-}
-
 /* tablet */
 @media only screen and (min-width: 760px) and (max-width: 1024px) {
-  .background {
-    background-image: url("../assets/images/destination/background-destination-tablet.jpg");
-  }
 }
 </style>
